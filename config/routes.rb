96a82417335c299d745b73f0b90f382d6152f541
vlsk3104 <vlsk3104@gmail.com>
2020-11-01 20:43:07 +0900

    応用課題２

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'homes#top'
+  get 'home/about' => 'homes#about', as: 'about'
+  devise_for :users
+  resources :users, only: [:show,:index,:edit,:update]
+  resources :books
+
+
+end
